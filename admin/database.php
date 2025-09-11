f00bb08d0a0209b0bf3c685c0e3c9032241d1ff2
mdrafm <121547347+mdrafm@users.noreply.github.com>
2025-09-11 11:28:15 +0530

    Update database.php

--- admin/database.php
+++ admin/database.php
@@ -5,8 +5,8 @@ class Database
 
        private $db_host = 'localhost';
        private $db_user = 'root';
-       private $db_pass = 'MdR@6%&!#$1';
-       private $db_name = 'mdrafm';
+       private $db_pass = '';
+       private $db_name = '';
        private $mysqli = '';
        public $base_url = '';
 
