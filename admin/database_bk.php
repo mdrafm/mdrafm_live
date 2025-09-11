abef2325472e526667e0600e63ebbfcc515cf464
mdrafm <121547347+mdrafm@users.noreply.github.com>
2025-09-11 11:49:55 +0530

    Update database_bk.php

--- admin/database_bk.php
+++ admin/database_bk.php
@@ -4,8 +4,8 @@
         
       private $db_host = 'localhost';
        private $db_user = 'root';
-       private $db_pass = 'MdR@6%&!#$1';
-       private $db_name = 'mdrafm';
+       private $db_pass = '';
+       private $db_name = '';
        private $mysqli = '';
        
        private $result = array();
@@ -224,5 +224,6 @@
          }
        }
    }
+
 
 ?>
